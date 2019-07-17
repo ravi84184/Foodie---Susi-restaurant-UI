a84b2648731584e559c98c744d4346939b6170b2
ravipatel <102030@@>
2019-07-17 17:54:41 +0530

    first commit

---
+++ app/src/main/java/com/nikdemo/foodie_susi_restorentui/Constants.java
@@ -0,0 +1,34 @@
+package com.nikdemo.foodie_susi_restorentui;
+
+/**
+ * Created by nehal on 22/11/18.
+ */
+public
+class Constants {
+
+
+    public static final String TOKEN = "token";
+    public static final String USER_ID = "user_id";
+    public static final String CHILD_ID = "child_id";
+    public static final String CHILD_SCHOOL_ID = "child_school_id";
+    public static final String CHILD_CLASS_ID = "child_class_id";
+    public static final String USER_PROFILE = "user_profile";
+    public static final String USER_EMAIL = "user_email";
+    public static final String USER_MOBILE = "user_mobile";
+    public static final String USER_NAME = "user_name";
+    public static final String USER_TYPE = "user_type";
+    public static final String USER_STATE_ID = "state_id";
+    public static final String USER_CITY_ID = "city_id";
+    public static final String USER_SCHOOL_ID = "school_id";
+    public static final String USER_INSTRUCTOR_ID = "instructor_id";
+    public static final String USER_SPORT_ID = "sport_id";
+
+
+    // Fonts
+    public static String sourceLatoRegular = "fonts/Lato-Regular.ttf";
+    public static String sourceLatoBold = "fonts/Lato-Bold.ttf";
+    public static String sourceTitilliumRegular = "fonts/Titillium-Regular.otf";
+    public static String sourceTitilliumBold = "fonts/Titillium-Bold.otf";
+
+
+}
